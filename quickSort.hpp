cacefeb0c4e637d7628f02aca3fd0210a142f51a
GeoGanias <geoganias@gmail.com>
2020-02-20 22:46:31 +0200

    somethiong+ qS

---
+++ quickSort.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void swap(int*, int*);  
+int partition (int arr[], int low, int high, int arr2[]);
+void quickSort(int arr[], int low, int high, int arr2[]);  
+void printArray(int arr[], int size);  
